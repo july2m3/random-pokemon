05801b337c45d59ebd3216013d637a51fb80d807
Julio <july2m3@gmail.com>
2020-04-13 16:25:31 -0500

    Initial project setup

---
+++ code.js
@@ -0,0 +1,24 @@
+/*
+Get a random pokemon using
+the pokemon api
+  https://pokeapi.co/docs/v2.html/
+*/
+
+const getRandomPokemon = () => {
+  let random = Math.floor(Math.random() * 800);
+  fetch(`https://pokeapi.co/api/v2/pokemon/${random}/`)
+    .then((response) => response.json())
+    .then((myJson) => {
+      let nameElement = document.querySelector('.name');
+      nameElement.innerText = myJson.name;
+
+      let imageUrl = myJson.sprites.front_default;
+      document.querySelector('img').src = imageUrl;
+    })
+    .catch((err) => console.log(err));
+};
+
+window.addEventListener('load', getRandomPokemon());
+
+const button = document.querySelector('button');
+button.addEventListener('click', getRandomPokemon);
